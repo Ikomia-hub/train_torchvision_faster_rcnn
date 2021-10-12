4b5b91b65e36545ee5c01f596c304101aa8ad7cb
Ludo <ludovic.barusseau@gmail.com>
2021-10-12 09:28:09 +0200

    Remove IkDatasetIO input from class inherited from TrainProcess (included in the base class)

--- train_torchvision_faster_rcnn_process.py
+++ train_torchvision_faster_rcnn_process.py
@@ -51,8 +51,6 @@ class TrainFasterRcnn(dnntrain.TrainProcess):
 
     def __init__(self, name, param):
         dnntrain.TrainProcess.__init__(self, name, param)
-        # Add input/output of the process here
-        self.addInput(datasetio.IkDatasetIO())
 
         # Create parameters class
         if param is None:
