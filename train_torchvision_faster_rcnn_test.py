33ac8923538a9ee79220da9d2b7bde108c6ec368
ambroise <ambroise.berthe@ikomia.com>
2022-06-27 10:36:26 +0200

    Fix test

--- train_torchvision_faster_rcnn_test.py
+++ train_torchvision_faster_rcnn_test.py
@@ -7,10 +7,10 @@ logger = logging.getLogger(__name__)
 
 def test(t, data_dict):
     logger.info("===== Test::train torchvision faster rcnn =====")
-    input_path = t.getInput(0)
+    input_dataset = t.getInput(0)
     params = task.get_parameters(t)
     params["epochs"] = 1
     params["batch_size"] = 2
     task.set_parameters(t, params)
-    input_path.setPath(data_dict["datasets"]["detection"]["dataset_coco"])
+    input_dataset.load(data_dict["datasets"]["detection"]["dataset_wgisd"])
     yield run_for_test(t)
